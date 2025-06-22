d7ac5939cfb626533dddef9aabbc37640f325b02
yiranyyu <2606375857@qq.com>
2025-06-22 21:08:29 +0800

    update name

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -2,8 +2,8 @@ set -x
 
 eval "$(conda shell.bash hook)"
 
-conda create -n viper python==3.10 -y
-conda activate viper
+conda create -n RLPR python==3.10 -y
+conda activate RLPR
 # pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -e .
 pip install flash-attn==2.7.4.post1
