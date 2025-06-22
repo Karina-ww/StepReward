551089e20c1781bd5c50a5e1126772fb289962db
季博 <gsorris27@gmail.com>
2025-06-23 03:30:18 +0800

    add scripts

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -7,3 +7,7 @@ conda activate RLPR
 # pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -e .
 pip install flash-attn==2.7.4.post1
+
+
+# pip install -U vllm==0.8.5
+# pip install tensordict==0.6.2
