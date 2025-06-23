667ab3636ddac54da61738106b6ae5bd599e0f6e
季博 <gsorris27@gmail.com>
2025-06-23 10:34:34 +0800

    env setup

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -9,5 +9,6 @@ pip install -e .
 pip install flash-attn==2.7.4.post1
 
 
-# pip install -U vllm==0.8.5
-# pip install tensordict==0.6.2
+# below is for gemma & llama
+pip install -U vllm==0.8.5
+pip install tensordict==0.6.2
